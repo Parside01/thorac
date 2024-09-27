7040c84347cd984eef17291d712d48107e29ed53
parside <vadimakovlev79449@gmail.com>
2024-09-28 00:28:10 +0700

    Немного поменял папочки)

--- backend/tasks/generate.go
+++ backend/tasks/generate.go
@@ -1,5 +1,5 @@
 package tasks
 
-//go:generate go run github.com/sqlc-dev/sqlc/cmd/sqlc@v1.27.0 vet --file=./database/erd/sqlc.yaml
-//go:generate go run github.com/sqlc-dev/sqlc/cmd/sqlc@v1.27.0 generate ./database/erd --file=./database/erd/sqlc.yaml
-//go:generate go run github.com/sqlc-dev/sqlc/cmd/sqlc@v1.27.0 diff ./database/erd --file=./database/erd/sqlc.yaml
+//go:generate go run github.com/sqlc-dev/sqlc/cmd/sqlc@v1.27.0 vet --file=./internal/database/erd/sqlc.yaml
+//go:generate go run github.com/sqlc-dev/sqlc/cmd/sqlc@v1.27.0 generate --file=./internal/database/erd/sqlc.yaml
+//go:generate go run github.com/sqlc-dev/sqlc/cmd/sqlc@v1.27.0 diff ./database/erd --file=./internal/database/erd/sqlc.yaml
